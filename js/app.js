980b9ab40655abfe4885a811bf5313f4d2b36bec
Azim <azimuddin807@gmail.com>
2024-09-22 01:00:40 +0600

    result section hide

--- js/app.js
+++ js/app.js
@@ -1,20 +1,69 @@
-
-    let incomeValue = inputValue('income');
-    let softwareValue = inputValue('software');
-    let courseValue = inputValue('courses');
-    let internetValue =inputValue('internet');
-
-   
+  
  document.getElementById('calculate').addEventListener('click',function(){
     
+    const incomeValue = inputValue('income');
+    const softwareValue = inputValue('software');
+    const courseValue = inputValue('courses');
+    const internetValue =inputValue('internet');
 
     const allExpense = softwareValue + courseValue + internetValue;
     const balance = incomeValue - allExpense;
     
     const totalExpense = textValue('total-expenses');
-     document.getElementById('total-expenses').innerText = allExpense;
+    document.getElementById('total-expenses').innerText = allExpense;
      document.getElementById('balance').innerText = balance;
 
-    
+        //Show results section
+      const resultArea = document.getElementById('results');
+       resultArea.classList.remove('hidden');
 
  })
+
+ document.getElementById('calculate-savings').addEventListener('click',function(){
+
+    const incomeValue = inputValue('income');
+    const softwareValue = inputValue('software');
+    const courseValue = inputValue('courses');
+    const internetValue =inputValue('internet');
+
+    const allExpense = softwareValue + courseValue + internetValue;
+    const balance = incomeValue - allExpense;
+
+    const saveValue = inputValue('savings');
+    const saveAmount = (balance * saveValue) / 100;
+    
+    document.getElementById('savings-amount').innerText = saveAmount;
+   
+    const remainBalance = balance - saveAmount;
+    // document.getElementById('remaining-balance');
+    document.getElementById('remaining-balance').innerText = remainBalance;
+
+
+
+ })
+
+ //Assistance Tab
+
+   const assistantTab = document.getElementById('assistant-tab');
+   assistantTab.addEventListener('click',function(){
+      const hsitoryTabItems = document.getElementById('history-tab');
+      hsitoryTabItems.classList.remove('py-2', 'rounded-l-md',  'duration-200', 'text-white',  'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+
+      assistantTab.classList.add('py-2', 'rounded-l-md',  'duration-200', 'text-white',  'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+   })
+
+ //History section
+
+   const hsitoryTab = document.getElementById('history-tab');
+   hsitoryTab.addEventListener('click',function(){
+      
+      hsitoryTab.classList.add('py-2', 'rounded-l-md', 'focus:outline-none', 'transition-colors', 'duration-200', 'text-white', 'font-semibold', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
+      
+      const asstiance = document.getElementById('assistant-tab');
+      asstiance.classList.remove('py-2', 'rounded-l-md',  'duration-200', 'text-white',  'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
+   })
+
+
+
+
+
