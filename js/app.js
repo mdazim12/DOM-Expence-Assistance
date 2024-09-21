e1e0d6c33dd0b9e58eda33d963a49a976cb90c5d
Azim <azimuddin807@gmail.com>
2024-09-21 21:24:44 +0600

    total expense add & updatred

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 
+    let incomeValue = inputValue('income');
+    let softwareValue = inputValue('software');
+    let courseValue = inputValue('courses');
+    let internetValue =inputValue('internet');
+
    
- 
+ document.getElementById('calculate').addEventListener('click',function(){
+    
+
+    const allExpense = softwareValue + courseValue + internetValue;
+    const balance = incomeValue - allExpense;
+    
+    const totalExpense = textValue('total-expenses');
+     document.getElementById('total-expenses').innerText = allExpense;
+     document.getElementById('balance').innerText = balance;
+
+    
+
+ })
---
+++ js/utilitiy.js
@@ -0,0 +1,12 @@
+
+    function inputValue(id){
+      const inputValueItems =  parseFloat(document.getElementById(id).value);
+      return inputValueItems;
+    }
+
+    function textValue(id){
+        const textValueItems = parseFloat(document.getElementById(id).innerText);
+        return textValueItems;
+    }
+
+   
